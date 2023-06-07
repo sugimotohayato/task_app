12e4d3fdc42eb6a8c761aecd3f05318e15448189
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-06-07 14:06:16 +0900

    投稿機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,54 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    puts params.inspect  # デバッグ出力
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクが作成されました。"
+    else
+      render :index
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクが更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクが削除されました。"
   end
 
   def destroy_multiple
+    task_ids = params[:task_ids]
+    Task.where(id: task_ids).destroy_all
+    redirect_to tasks_path, notice: "選択したタスクが削除されました。"
   end
 
+  private
+
+  def task_params
+    params.permit(:date, :title)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+
+  validates :date, presence: true
+  validates :title, presence: true
+
 end
